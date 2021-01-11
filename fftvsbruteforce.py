d6ad8f261fe8490163be42021c1dd26600273ee1
EvLies <77052633+EvLies@users.noreply.github.com>
2021-01-11 17:49:50 +0100

    Add files via upload

---
+++ fftvsbruteforce.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 16:22:34 2021
+
+@author: Evanl
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from math import gcd
+from scipy.signal import find_peaks
+import time
+
+
+def fft_primefactor(a,num_it,N):
+    for i in range(num_it):
+        #print(i)
+        while gcd(a,N) != 1:
+            a+=1
+        x = np.arange(20,dtype=object)
+        fx = a**x % N
+        freq = np.fft.rfftfreq(len(x))[1:-1]
+        fft = np.abs(np.fft.rfft(fx, norm="ortho")[1:-1])
+        r = np.round(1/(freq[(np.max(fft)==fft)]))
+        if not isinstance(r, int): 
+            r=r[0]
+        
+        if (r % 2):
+            a+=1    
+        else:
+            x = int(a**(r/2) % N)
+            if (x + 1) % N != 0:
+                p,q = gcd(x+1,N), gcd(x-1,N)
+                if (p*q==N) and p>1 and q>1:
+                    return [p,q]
+                else:
+                    a+=1
+            else:
+                a+=1
+        if i == num_it-1:
+            print("ran out of iterations")
+            return
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+N=29*31
+a=2
+iterations = 100
+
+time1=time.time()
+print(fft_primefactor(a,iterations,N))
+time2=time.time()
+time3=time.time()
+print(prime_factors(N))
+time4 = time.time()
+print(time2-time1)
+print(time4-time3)
+
+
+
+"""
+#BENCHMARK
+time1=time.time()
+for i in range(100):
+    #print(fft_primefactor(a,iterations,N))
+    fft_primefactor(a,iterations,N)
+time2=time.time()
+time3=time.time()
+for i in range(100):
+    #print(prime_factors(N))
+    prime_factors(N)
+time4 = time.time()
+print(time2-time1)
+print(time4-time3)
+"""
+
